ef5a2a305736332eeb8aef0446f0f8c0240879e3
jlamaspi <jlamaspi@LIM-8JH59K3.usersad.everis.int>
2022-03-14 12:12:36 -0500

    Feat: Initialize project

---
+++ public_service-debit_public_service-debit/src/main/java/com/bootcamp/public_servicedebit/PublicServiceDebitApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.public_servicedebit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicServiceDebitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicServiceDebitApplication.class, args);
+	}
+
+}
---
+++ public_service-debit_public_service-debit/src/test/java/com/bootcamp/public_servicedebit/PublicServiceDebitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.public_servicedebit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicServiceDebitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
